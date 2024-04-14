b06ddccd29a485d9300f0516b95221abee4ca724
unknown <sourav.ece9@gmail.com>
2024-04-14 16:10:57 +0530

    Initial Commit

--- 8-promises.js
+++ 8-promises.js
@@ -1,18 +1,66 @@
-const doWorkPromise = new Promise((resolve, reject) =>
-{
-    setTimeout(() =>
-    {
-        reject ("Things went wrong!!");
+// const doWorkPromise = new Promise((resolve, reject) =>
+// {
+//     setTimeout(() =>
+//     {
+//         resolve([ 1, 4, 7 ]);
         
-        // resolve([ 1, 4, 7 ]);
-    }, 2000);
-});
+//         // reject ("Things went wrong!!");
+        
+//     }, 2000);
+// });
 
-doWorkPromise.then((result) =>
+// doWorkPromise.then((result) =>
+// {
+//     console.log("Success!!", result);
+// })
+// .catch((error) =>
+// {
+//     console.log("Error!!", error);
+// });
+
+
+const add = (a, b) =>
 {
-    console.log("Success!!", result);
+    return new Promise((resolve, reject) =>
+    {
+        setTimeout(() =>
+        {
+            resolve(a + b);
+        }, 0);
+    });
+}
+
+// add(1, 2)
+// .then((sum) =>
+// {
+//     console.log(sum);
+    
+//     add(sum, 5)
+//     .then((sum2) =>
+//     {
+//         console.log(sum2);
+//     })
+//     .catch((error2) =>
+//     {
+//         console.log(error2);
+//     });
+// })
+// .catch((error) =>
+// {
+//     console.log(error);
+// });
+
+add(1, 1)
+.then((sum) =>
+{
+    console.log(sum);
+    return add(sum, 4);
+})
+.then((sum2) =>
+{
+    console.log(sum2);
 })
 .catch((error) =>
 {
-    console.log("Error!!", error);
+    console.log(error);
 });
